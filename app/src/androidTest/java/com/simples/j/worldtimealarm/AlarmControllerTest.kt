386f0c72c317412b2f3d49aa85794e69f700c379
James Yang <md2ybh@gmail.com>
2019-03-07 19:31:17 +0900

    - add test and change class name

--- app/src/androidTest/java/com/simples/j/worldtimealarm/AlarmControllerTest.kt
+++ app/src/androidTest/java/com/simples/j/worldtimealarm/AlarmControllerTest.kt
@@ -18,7 +18,7 @@ import java.util.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AlarmControllerTest {
 
     private lateinit var context: Context
 
